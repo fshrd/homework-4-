54c5914a553eaa40684b79665b9e4bd24cbba7fc
fshrd <fashrod771@gmail.com>
2024-10-13 22:49:00 +0300

    ГОООЛ

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var s:= readinteger('Ты кто? (Русский - 1; EnglishMan - 2; Еврей - 3). Ваш ввод:');
+  Assert(s > 0);
+  case s of
+    1: println('Доброе утро, мир!');
+    2: println('Hello world!');
+    3: println('שלום עולם!');
+    else println('Я не понимать ваш language.');
+  end;
+end.
